bafa89ecbd2ed89efd17fc302fcd089ad13e032e
liufee <job@feehi.com>
2026-05-06 09:21:53 +0800

    fix

--- src/pages/exercise/record.tsx
+++ src/pages/exercise/record.tsx
@@ -39,7 +39,7 @@ export default function RecordScreen() {
         [RecordType.RecordTypeAbdominal, RecordType.RecordTypeRun, RecordType.RecordTypeSitUpPushUp],
         showRecordStart,
         showRecordEnd,
-        'asc'
+        'desc'
       );
       if (!success) {
         Alert.alert('失败', error);
