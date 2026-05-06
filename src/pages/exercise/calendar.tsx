5689d0178bd46fc5097b72a29a79dc6480f9e355
liufee <job@feehi.com>
2026-05-06 08:46:41 +0800

    fix

--- src/pages/exercise/calendar.tsx
+++ src/pages/exercise/calendar.tsx
@@ -1,198 +1,218 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { format, startOfMonth, endOfMonth, eachDayOfInterval, isSameMonth, isSameDay, parse } from 'date-fns';
-import { zhCN } from 'date-fns/locale';
-import { DailyExercise } from '@/src/service/exercise/model';
+import {Alert, Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {Calendar} from 'react-native-calendars';
+import React, {useState} from 'react';
+import {format, parse} from 'date-fns';
+import {useNavigation} from '@react-navigation/native';
+import {Record as RecordModel, RecordType} from '../../service/exercise/model';
+import {exerciseService} from '../../service/exercise/exercise';
 
-interface CalendarProps {
-  records: DailyExercise[];
+interface RecordsByDate{
+  date:string,
+  exercises: RecordModel[],
 }
 
-export default function Calendar({ records }: CalendarProps) {
-  const [currentDate, setCurrentDate] = useState(new Date());
+const calendar = ({records}:{records:RecordsByDate[]}) => {
+  const [selectedDate, setSelectedDate] = useState<RecordsByDate|null>(null);
+  const navigation = useNavigation();
 
-  const monthStart = startOfMonth(currentDate);
-  const monthEnd = endOfMonth(currentDate);
-  const days = eachDayOfInterval({ start: monthStart, end: monthEnd });
-
-  // Create a map of date to exercise data
-  const exerciseMap = new Map<string, DailyExercise>();
-  records.forEach(record => {
-    exerciseMap.set(record.date, record);
-  });
-
-  const prevMonth = () => {
-    setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1));
+  const getMarkedDates = () => {
+    let markedDates = {};
+    for(let i in records){
+      let types = [];
+      for(let j in records[i].exercises){
+        types.push(records[i].exercises[j].type);
+      }
+      const valueSet = new Set(types);
+      if( [RecordType.RecordTypeAbdominal, RecordType.RecordTypeRun, RecordType.RecordTypeSitUpPushUp].every(val => valueSet.has(val)) ){
+        markedDates[records[i].date] = {
+          customStyles: {
+            container: {
+              backgroundColor: '#4CAF50',
+              borderRadius: 5,
+              justifyContent: 'center',
+              alignItems: 'center',
+            },
+            text: { color: 'white', fontWeight: 'bold' },
+          },
+        };
+      }else {
+        markedDates[records[i].date] = {
+          customStyles: {
+            container: {
+              backgroundColor: '#FFB74D',
+              borderRadius: 5,
+              justifyContent: 'center',
+              alignItems: 'center',
+            },
+            text: { color: 'white', fontWeight: 'bold' },
+          },
+        };
+      }
+    }
+    return markedDates;
   };
-
-  const nextMonth = () => {
-    setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1));
+  const handleDayPress = (day) => {
+    const selectedDay = records.find(item => item.date === day.dateString);
+    setSelectedDate(selectedDay || null);
   };
-
-  const getBadgeStatus = (record?: DailyExercise) => {
-    if (!record) return 'none';
-    return record.allCompleted ? 'success' : 'partial';
+  const handleExerciseClick = (exercise) => {
+    if(exercise.type === RecordType.RecordTypeRun){
+      navigation.navigate('RecordDetailRunTracker' as any, {id: exercise.id} as any)
+    }
   };
+  const handleDelete = (record) => {
+    Alert.alert('确认删除', '确定要删除这个记录吗？', [
+      { text: '取消', style: 'cancel' },
+      {
+        text: '删除',
+        style: 'destructive',
+        onPress: async () => {
+          const[success, err] = await exerciseService.deleteRecord(record);
+          if(!success){
+            Alert.alert('失败', err);
+            return;
+          }
+        },
+      },
+    ]);
+  };
+  return(
+      <View style={styles.container}>
+        <View style={styles.content}>
+          <View style={ {flexDirection: 'row', alignItems: 'center'}}>
+            <View style={{backgroundColor: '#4CAF50',width: 20,height: 20,justifyContent: 'center',alignItems: 'center',borderRadius: 3}}></View>
+            <Text style={{color: 'black', fontSize: 13, fontWeight: 'bold',}}> 当天所有运动已完成 </Text>
+          </View>
+          <View style={{flexDirection: 'row', alignItems: 'center', marginTop:10}}>
+            <View style={{backgroundColor: '#FFB74D',width: 20,height: 20,justifyContent: 'center',alignItems: 'center',borderRadius: 3}}></View>
+            <Text style={{color: 'black', fontSize: 13, fontWeight: 'bold',}}> 当天部分运动完成 </Text>
+          </View>
+          <Calendar
+              current={new Date().toISOString().split('T')[0]} // 当前日期
+              markedDates={getMarkedDates()} // 标记锻炼过的日期
+              onDayPress={handleDayPress}
+              markingType={'custom'}
+              monthFormat={'yyyy年MM月'}
+              theme={{
+                selectedDayBackgroundColor: '#4CAF50',
+                todayTextColor: '#00adf5',
+              }}
+          />
+          {selectedDate && (
+              <View style={styles.selectedDateContainer}>
+                <Text style={styles.selectedDateText}>
+                  <View style={{width:'100%'}}><Text>{selectedDate.date}</Text></View>
+                  {selectedDate.exercises.map((exercise, idx) => {
+                    const startAt = parse(exercise.startAt, 'yyyy-MM-dd HH:mm:ss', new Date());
+                    const endAt = parse(exercise.endAt, 'yyyy-MM-dd HH:mm:ss', new Date());
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <TouchableOpacity onPress={prevMonth}>
-          <Text style={styles.navButton}>{'<'}</Text>
-        </TouchableOpacity>
-        <Text style={styles.monthTitle}>
-          {format(currentDate, 'yyyy年MM月', { locale: zhCN })}
-        </Text>
-        <TouchableOpacity onPress={nextMonth}>
-          <Text style={styles.navButton}>{'>'}</Text>
-        </TouchableOpacity>
-      </View>
-
-      <View style={styles.weekdays}>
-        {['日', '一', '二', '三', '四', '五', '六'].map((day, index) => (
-          <Text key={index} style={styles.weekday}>
-            {day}
-          </Text>
-        ))}
-      </View>
-
-      <View style={styles.daysGrid}>
-        {/* Empty cells for days before month starts */}
-        {Array.from({ length: monthStart.getDay() }).map((_, index) => (
-          <View key={`empty-${index}`} style={styles.dayCell} />
-        ))}
-
-        {days.map((day, index) => {
-          const dateStr = format(day, 'yyyy-MM-dd');
-          const exercise = exerciseMap.get(dateStr);
-          const badgeStatus = getBadgeStatus(exercise);
-          const isToday = isSameDay(day, new Date());
-
-          return (
-            <View key={index} style={styles.dayCell}>
-              <View
-                style={[
-                  styles.dayNumber,
-                  isToday && styles.today,
-                  badgeStatus === 'success' && styles.successDay,
-                  badgeStatus === 'partial' && styles.partialDay,
-                ]}>
-                <Text
-                  style={[
-                    styles.dayText,
-                    !isSameMonth(day, currentDate) && styles.otherMonth,
-                  ]}>
-                  {format(day, 'd')}
+                    if(exercise.type === RecordType.RecordTypeRun && exercise.run.runningWithoutPosition === 1){
+                      exercise.run.distance = 8.15;
+                      const [hours, minutes, seconds] = exercise.run.runDuration.split(':').map(Number);
+                      const totalHours = hours + minutes / 60 + seconds / 3600; // 转换为小时
+                      exercise.run.avgPace = exercise.run.distance / totalHours;
+                    }
+                    return(
+                        <TouchableOpacity
+                            key={idx}
+                            style={styles.exerciseCard}
+                            onPress={()=>handleExerciseClick(exercise)} // Navigate to the details page on click
+                            onLongPress={()=>handleDelete(exercise)}
+                        >
+                          <View style={{...styles.iconContainer}}>
+                            {exercise.type === RecordType.RecordTypeAbdominal && <Text><Image source={require('../../../assets/images/abdominal.png')} width={40} height={40} /></Text>}
+                            {exercise.type === RecordType.RecordTypeRun && <Text><Image source={require('../../../assets/images/run_tracker.png')} width={40} height={40} /></Text>}
+                            {exercise.type === RecordType.RecordTypeSitUpPushUp && <Text style={{position:'relative', top:-12}}><Image source={require('../../../assets/images/sit-up.png')} width={40} height={40} /></Text>}
+                          </View>
+                          <View style={styles.detailsContainer}>
+                            {exercise.type === RecordType.RecordTypeAbdominal &&
+                                <Text style={styles.exerciseText}>{`${format(startAt, 'HH:mm:ss')}~${format(endAt, 'HH:mm:ss')} / ${((endAt.getTime()-startAt.getTime())/1000/60).toFixed(2)}min`}</Text>
+                            }
+                            {exercise.type === RecordType.RecordTypeRun &&
+                                <Text style={styles.exerciseText}>{`${format(startAt, 'HH:mm:ss')}~${format(endAt, 'HH:mm:ss')} | 配速: ${exercise.run.avgPace.toFixed(2)}km/h | 耗时: ${exercise.run.runDuration} | 距离: ${exercise.run.distance.toFixed(2)}km ${exercise.run.runningWithoutPosition === 1 ? '| 仅计时' : '| 定位'}`}</Text>
+                            }
+                            {exercise.type === RecordType.RecordTypeSitUpPushUp &&
+                                <Text style={styles.exerciseText}>{`${format(startAt, 'HH:mm:ss')}~${format(endAt, 'HH:mm:ss')} / ${((endAt.getTime() - startAt.getTime()) / 1000 / 60).toFixed(2)}min | 俯卧撑: ${exercise.sitUpPushUp.pushUp} | 仰卧起坐: ${exercise.sitUpPushUp.sitUp} | 曲腿卷腹: ${exercise.sitUpPushUp.curlUp} | 靠墙倒立: ${exercise.sitUpPushUp.legsUpTheWallPose}`}</Text>
+                            }
+                          </View>
+                        </TouchableOpacity>
+                    )})}
                 </Text>
               </View>
-              {exercise && (
-                <View style={styles.badgeContainer}>
-                  <View
-                    style={[
-                      styles.badge,
-                      badgeStatus === 'success' ? styles.successBadge : styles.partialBadge,
-                    ]}>
-                    <Text style={styles.badgeText}>
-                      {badgeStatus === 'success' ? '✓' : exercise.exercises.length}
-                    </Text>
-                  </View>
-                </View>
-              )}
-            </View>
-          );
-        })}
+          )}
+        </View>
       </View>
-    </View>
   );
-}
+};
+export default calendar;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  content: {
     backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
+    width: '100%',
   },
-  header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 15,
-    paddingHorizontal: 10,
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
   },
-  navButton: {
-    fontSize: 24,
-    color: '#007bff',
+  modalText: {
+    fontSize: 16,
+    color: '#333',
+    marginBottom: 10,
+  },
+  modalButton: {
+    backgroundColor: '#FF5722',
     padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
   },
-  monthTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
+  modalButtonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  selectedDateContainer: {
+    marginTop: 20,
+  },
+  selectedDateText: {
+    fontSize: 16,
     color: '#333',
   },
-  weekdays: {
+  exerciseCard: {
     flexDirection: 'row',
-    marginBottom: 5,
+    alignItems: 'center',
+    marginBottom: 8,
+    backgroundColor: '#F9F9F9',
+    borderRadius: 10,
+    padding: 8,
+    shadowColor: '#ddd',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 6,
+    elevation: 2,
   },
-  weekday: {
+  iconContainer: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 10,
+  },
+  detailsContainer: {
     flex: 1,
-    textAlign: 'center',
-    fontSize: 12,
-    color: '#666',
-    fontWeight: '600',
-  },
-  daysGrid: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-  },
-  dayCell: {
-    width: '14.28%',
-    aspectRatio: 1,
-    padding: 2,
-    alignItems: 'center',
     justifyContent: 'center',
   },
-  dayNumber: {
-    width: 30,
-    height: 30,
-    borderRadius: 15,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  today: {
-    backgroundColor: '#007bff',
-  },
-  successDay: {
-    backgroundColor: '#4CAF50',
-  },
-  partialDay: {
-    backgroundColor: '#FFB74D',
-  },
-  dayText: {
+  exerciseText: {
     fontSize: 14,
-    color: '#333',
-  },
-  otherMonth: {
-    color: '#ccc',
-  },
-  badgeContainer: {
-    marginTop: 2,
-  },
-  badge: {
-    width: 18,
-    height: 18,
-    borderRadius: 9,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  successBadge: {
-    backgroundColor: '#4CAF50',
-  },
-  partialBadge: {
-    backgroundColor: '#FFB74D',
-  },
-  badgeText: {
-    color: 'white',
-    fontSize: 10,
-    fontWeight: 'bold',
+    color: '#555',
+    lineHeight: 18,
   },
 });
