496edb1a0443e762634b92a617453e1d1c4ba821
liufee <job@feehi.com>
2026-05-07 09:41:47 +0800

    fix

---
+++ src/pages/exercise/tsrVerify.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import TsrVerify from '../../components/tsrVerify';
+import {exerciseService} from '../../service/exercise/exercise';
+import {Record} from '../../service/exercise/model';
+
+const TSRVerifyScreen = ({ route }: any) => {
+    const { type, exercise }:{type:string, exercise:Record} = route.params;
+
+
+    const formula = 'type+startAt+endAt+ext+paths';
+    const createdAt = exercise.startAt;
+
+    return <TsrVerify formula={formula} createdAt={createdAt}
+                      getFullOriginalString={
+                          async ()=>{
+                              const [success, result] = await exerciseService.assembleStrToCreateTSR(exercise.id);
+                              if(success){
+                                  return [true, result, ''];
+                              }else{
+                                  return [false, '', result];
+                              }
+                          }
+                      }
+                      getTSR={
+                          async ()=>{
+                              const [success, tsr, err] = await exerciseService.getTSR(exercise.id);
+                              if(success){
+                                  return [true, tsr, ''];
+                              }else{
+                                  return [false, '', err];
+                              }
+                          }
+                      }
+
+    ></TsrVerify>;
+};
+
+export default TSRVerifyScreen;
