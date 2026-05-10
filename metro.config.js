20674c4112d9e0f0c33230a811a6b0908199d175
liufee <job@feehi.com>
2026-05-10 09:02:23 +0800

    feat: use FeehiApp as feehi

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,25 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+const projectRoot = __dirname;
+
+config.watchFolders = [
+    '/Users/lf/work/js/FeehiApp/src',
+];
+
+config.resolver.unstable_enableSymlinks = true;
+
+config.resolver.nodeModulesPaths = [
+    path.resolve(projectRoot, 'node_modules'),
+];
+
+config.resolver.extraNodeModules = {
+    react: path.resolve(projectRoot, 'node_modules/react'),
+    'react-native': path.resolve(projectRoot, 'node_modules/react-native'),
+};
 
 // 重点：允许 Metro 解析 .wasm 文件作为静态资源
 config.resolver.assetExts.push('wasm');
